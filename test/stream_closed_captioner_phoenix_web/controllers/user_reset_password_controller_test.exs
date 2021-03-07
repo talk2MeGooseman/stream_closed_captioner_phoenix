2dd5606c2216d15638d7c13def28215fbcd67012
Erik Guzman <erik.guzman@guzman.codes>
2021-03-07 21:24:17 +0000

    Enable and update user reset password

--- test/stream_closed_captioner_phoenix_web/controllers/user_reset_password_controller_test.exs
+++ test/stream_closed_captioner_phoenix_web/controllers/user_reset_password_controller_test.exs
@@ -11,7 +11,6 @@ defmodule StreamClosedCaptionerPhoenixWeb.UserResetPasswordControllerTest do
 
   describe "GET /users/reset_password" do
 
-    @tag :skip
     test "renders the reset password page", %{conn: conn} do
       conn = get(conn, Routes.user_reset_password_path(conn, :new))
       response = html_response(conn, 200)
@@ -22,7 +21,6 @@ defmodule StreamClosedCaptionerPhoenixWeb.UserResetPasswordControllerTest do
   describe "POST /users/reset_password" do
     # @tag :capture_log
 
-    @tag :skip
     test "sends a new reset password token", %{conn: conn, user: user} do
       conn =
         post(conn, Routes.user_reset_password_path(conn, :create), %{
@@ -34,7 +32,6 @@ defmodule StreamClosedCaptionerPhoenixWeb.UserResetPasswordControllerTest do
       assert Repo.get_by!(Accounts.UserToken, user_id: user.id).context == "reset_password"
     end
 
-    @tag :skip
     test "does not send reset password token if email is invalid", %{conn: conn} do
       conn =
         post(conn, Routes.user_reset_password_path(conn, :create), %{
@@ -57,13 +54,11 @@ defmodule StreamClosedCaptionerPhoenixWeb.UserResetPasswordControllerTest do
       %{token: token}
     end
 
-    @tag :skip
     test "renders reset password", %{conn: conn, token: token} do
       conn = get(conn, Routes.user_reset_password_path(conn, :edit, token))
       assert html_response(conn, 200) =~ "Reset Password</h5>"
     end
 
-    @tag :skip
     test "does not render reset password with invalid token", %{conn: conn} do
       conn = get(conn, Routes.user_reset_password_path(conn, :edit, "oops"))
       assert redirected_to(conn) == "/"
@@ -81,7 +76,6 @@ defmodule StreamClosedCaptionerPhoenixWeb.UserResetPasswordControllerTest do
       %{token: token}
     end
 
-    @tag :skip
     test "resets password once", %{conn: conn, user: user, token: token} do
       conn =
         put(conn, Routes.user_reset_password_path(conn, :update, token), %{
@@ -97,23 +91,21 @@ defmodule StreamClosedCaptionerPhoenixWeb.UserResetPasswordControllerTest do
       assert Accounts.get_user_by_email_and_password(user.email, "new valid password")
     end
 
-    @tag :skip
     test "does not reset password on invalid data", %{conn: conn, token: token} do
       conn =
         put(conn, Routes.user_reset_password_path(conn, :update, token), %{
           "user" => %{
-            "password" => "too short",
+            "password" => "6 plz",
             "password_confirmation" => "does not match"
           }
         })
 
       response = html_response(conn, 200)
       assert response =~ "Reset Password</h5>"
-      assert response =~ "should be at least 12 character(s)"
+      assert response =~ "should be at least 6 character(s)"
       assert response =~ "does not match password"
     end
 
-    @tag :skip
     test "does not reset password with invalid token", %{conn: conn} do
       conn = put(conn, Routes.user_reset_password_path(conn, :update, "oops"))
       assert redirected_to(conn) == "/"
