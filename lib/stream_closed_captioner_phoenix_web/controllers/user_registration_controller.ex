46a30c922b45cfcef2f4f9fd35b7daf02252880b
Erik Guzman <erik.guzman@guzman.codes>
2021-03-09 04:35:25 +0000

    fix user settings and remove registration confirm

--- lib/stream_closed_captioner_phoenix_web/controllers/user_registration_controller.ex
+++ lib/stream_closed_captioner_phoenix_web/controllers/user_registration_controller.ex
@@ -10,15 +10,10 @@ defmodule StreamClosedCaptionerPhoenixWeb.UserRegistrationController do
     render(conn, "new.html", changeset: changeset)
   end
 
+  @spec create(Plug.Conn.t(), map) :: Plug.Conn.t()
   def create(conn, %{"user" => user_params}) do
     case Accounts.register_user(user_params) do
       {:ok, user} ->
-        {:ok, _} =
-          Accounts.deliver_user_confirmation_instructions(
-            user,
-            &Routes.user_confirmation_url(conn, :confirm, &1)
-          )
-
         conn
         |> put_flash(:info, "User created successfully.")
         |> UserAuth.log_in_user(user)
