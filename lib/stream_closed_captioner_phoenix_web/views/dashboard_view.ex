b8d4e4dfb13062802a8fe8daf2fc80ddc3fd0b7a
Erik Guzman <erik.guzman@codingzeal.com>
2021-05-09 07:01:54 +0000

    Tweak UI

--- lib/stream_closed_captioner_phoenix_web/views/dashboard_view.ex
+++ lib/stream_closed_captioner_phoenix_web/views/dashboard_view.ex
@@ -1,3 +1,8 @@
 defmodule StreamClosedCaptionerPhoenixWeb.DashboardView do
   use StreamClosedCaptionerPhoenixWeb, :view
+
+  def display_translation_status(translation_active) when is_nil(translation_active),
+    do: "Disabled"
+
+  def display_translation_status(translation_active), do: "Enabled"
 end
