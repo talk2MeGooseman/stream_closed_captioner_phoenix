01888ff1a2ddc40924b4fbd3bf221adf976f3337
Erik Guzman <erik.guzman@codingzeal.com>
2021-05-23 06:35:01 +0000

    Actually use bamboo mailer and update UI

--- lib/stream_closed_captioner_phoenix/accounts/user_notifier.ex
+++ lib/stream_closed_captioner_phoenix/accounts/user_notifier.ex
@@ -1,21 +1,24 @@
 defmodule StreamClosedCaptionerPhoenix.Accounts.UserNotifier do
-  # For simplicity, this module simply logs messages to the terminal.
-  # You should replace it by a proper email or notification tool, such as:
-  #
-  #   * Swoosh - https://hexdocs.pm/swoosh
-  #   * Bamboo - https://hexdocs.pm/bamboo
-  #
-  defp deliver(to, body) do
+  import Bamboo.Email
+
+  defp deliver(to, subject, body) do
     require Logger
     Logger.debug(body)
-    {:ok, %{to: to, body: body}}
+
+    new_email(
+      to: to,
+      from: "no-reply@gooseman.codes",
+      subject: subject,
+      text_body: body
+    )
+    |> StreamClosedCaptionerPhoenix.Mailer.deliver_now()
   end
 
   @doc """
   Deliver instructions to confirm account.
   """
   def deliver_confirmation_instructions(user, url) do
-    deliver(user.email, """
+    deliver(user.email, "Confirm your account", """
 
     ==============================
 
@@ -35,7 +38,7 @@ defmodule StreamClosedCaptionerPhoenix.Accounts.UserNotifier do
   Deliver instructions to reset a user password.
   """
   def deliver_reset_password_instructions(user, url) do
-    deliver(user.email, """
+    deliver(user.email, "Stream CC Password Reset Instructions", """
 
     ==============================
 
@@ -55,7 +58,7 @@ defmodule StreamClosedCaptionerPhoenix.Accounts.UserNotifier do
   Deliver instructions to update a user email.
   """
   def deliver_update_email_instructions(user, url) do
-    deliver(user.email, """
+    deliver(user.email, "Stream CC Update Email Instructions", """
 
     ==============================
 
