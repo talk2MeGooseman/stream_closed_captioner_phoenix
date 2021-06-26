2459b708f89edc15d54cec271435868805f9dd02
Erik Guzman <erik.guzman@guzman.codes>
2021-06-26 22:00:27 +0000

    Fix emailer tests

--- test/support/fixtures/accounts_fixtures.ex
+++ test/support/fixtures/accounts_fixtures.ex
@@ -20,8 +20,8 @@ defmodule StreamClosedCaptionerPhoenix.AccountsFixtures do
   end
 
   def extract_user_token(fun) do
-    {:ok, captured} = fun.(&"[TOKEN]#{&1}[TOKEN]")
-    [_, token, _] = String.split(captured.body, "[TOKEN]")
+    captured = fun.(&"[TOKEN]#{&1}[TOKEN]")
+    [_, token, _] = String.split(captured.text_body, "[TOKEN]")
     token
   end
 end
