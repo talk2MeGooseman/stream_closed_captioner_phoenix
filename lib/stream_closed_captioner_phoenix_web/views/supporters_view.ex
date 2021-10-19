3be3b0d299f173d2123a1871ce7ff9d047f94b1f
Erik Guzman <erik.guzman@guzman.codes>
2021-10-19 04:59:51 +0000

    Further style and filter twitch and patreon info

--- lib/stream_closed_captioner_phoenix_web/views/supporters_view.ex
+++ lib/stream_closed_captioner_phoenix_web/views/supporters_view.ex
@@ -1,3 +1,17 @@
 defmodule StreamClosedCaptionerPhoenixWeb.SupportersView do
   use StreamClosedCaptionerPhoenixWeb, :view
+
+  @spec filter_twitch_subscribers(list()) :: list()
+  def filter_twitch_subscribers(subscribes) do
+    Enum.filter(subscribes, fn x -> x["user"]["displayName"] != "Talk2meGooseman" end)
+  end
+
+  @spec filter_patreon_subscribers(list()) :: list()
+  def filter_patreon_subscribers(subscribes) do
+    Enum.filter(subscribes, fn x -> !is_nil(x["patronStatus"]) end)
+  end
+
+  def snakeCaseToText(text) do
+    String.replace(text, "_", " ")
+  end
 end
