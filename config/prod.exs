29161715d9684b15f9a3e8c92813e3439a98c4f2
Erik Guzman <erik.guzman@codingzeal.com>
2023-05-21 01:23:58 -0700

    Dont require ssl

--- config/prod.exs
+++ config/prod.exs
@@ -44,9 +44,6 @@ config :logger, level: :info
 # We also recommend setting `force_ssl` in your endpoint, ensuring
 # no data is ever sent via http, always redirecting to https:
 
-config :stream_closed_captioner_phoenix, StreamClosedCaptionerPhoenixWeb.Endpoint,
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
-
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
 
@@ -91,12 +88,14 @@ live_signing_salt =
     """
 
 config :stream_closed_captioner_phoenix, StreamClosedCaptionerPhoenixWeb.Endpoint,
+  # force_ssl: [rewrite_on: [:x_forwarded_proto]]
   server: true,
   http: [
     ip: {0, 0, 0, 0, 0, 0, 0, 0},
     port: String.to_integer(System.get_env("PORT") || "4000")
   ],
-  url: [host: System.get_env("HOST"), port: 443, scheme: "https"],
+  # url: [host: System.get_env("HOST"), port: 443, scheme: "https"],
+  url: [host: System.get_env("HOST")],
   secret_key_base: secret_key_base,
   live_view: [signing_salt: live_signing_salt]
 
