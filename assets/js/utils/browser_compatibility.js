2ce37d6e9330b1063489ff90a1e89d035065584e
Erik Guzman <erik.guzman@guzman.codes>
2023-10-19 22:19:30 +0000

    Remove safari check

--- assets/js/utils/browser_compatibility.js
+++ assets/js/utils/browser_compatibility.js
@@ -1,6 +1,5 @@
 export const isBrowserCompatible = () => {
   if (window.navigator.brave) return false;
-  if (window.navigator.userAgent.match(/Safari/).length > 0) return false;
 
   return !!(
     window.SpeechRecognition
