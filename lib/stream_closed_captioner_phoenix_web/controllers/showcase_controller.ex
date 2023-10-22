b37dc82805b1992e5b6f6f10eb9d95e65666c4a8
Erik Guzman <erik.guzman@guzman.codes>
2023-10-22 16:27:19 +0000

    Update showcase with user tracker method usage

--- lib/stream_closed_captioner_phoenix_web/controllers/showcase_controller.ex
+++ lib/stream_closed_captioner_phoenix_web/controllers/showcase_controller.ex
@@ -3,9 +3,7 @@ defmodule StreamClosedCaptionerPhoenixWeb.ShowcaseController do
 
   @spec index(Plug.Conn.t(), any) :: Plug.Conn.t()
   def index(conn, _params) do
-    active_channel_ids =
-      StreamClosedCaptionerPhoenixWeb.UserTracker.recently_active_channels()
-      |> Enum.map(fn {uid, _} -> uid end)
+    active_channel_ids = StreamClosedCaptionerPhoenixWeb.UserTracker.recently_active_channels()
 
     # Fetch information about the channel to display for Twitch API
     stream_list = Twitch.get_live_streams(active_channel_ids)
