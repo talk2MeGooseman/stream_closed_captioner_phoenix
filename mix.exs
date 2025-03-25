95fe3d3ecb258a5548ee05f0246d6c1b2e6758db
Erik Guzman <erik.guzman@guzman.codes>
2025-03-25 23:18:00 +0000

    Update elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule StreamClosedCaptionerPhoenix.MixProject do
     [
       app: :stream_closed_captioner_phoenix,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.16",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
